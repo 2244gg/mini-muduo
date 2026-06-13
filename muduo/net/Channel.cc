fc70f0302efa85308e12b0f6ac2ce9e928f65192
Ubuntu <ubuntu@localhost.localdomain>
2026-06-13 18:23:06 +0800

    add Channel.cc

--- muduo/net/Channel.cc
+++ muduo/net/Channel.cc
@@ -1,10 +1,49 @@
 #include "Channel.h"
-
-
+#include <sys/epoll.h>
 Channel::Channel(int fd)
 {
+    fd_=fd;
+}
+
+int Channel::fd() const{
+    return fd_;
+}
+
+uint32_t Channel::events() const{
+    return events_;
+}
+uint32_t Channel::set_revents(uint32_t revt){
+    revents_=revt;
+    return revents_;
 }
 
 void Channel::setReadCallback(EventCallback cb){
+    readCallback_=cb;
+}
+void Channel::setWriteCallback(EventCallback cb){
+    writeCallback_=cb;
+}
+void Channel::setCloseCallback(EventCallback cb){
+    closeCallback_=cb;
+}
+void Channel::setErrorCallback(EventCallback cb){
+    errorCallback_=cb;
+}
 
+void Channel::handleEvent(){
+    if(events_&revents_==0) return;
+    switch(revents_){
+        case EPOLLIN:
+            readCallback_();
+        break;
+        case EPOLLOUT:
+            writeCallback_();
+        break;
+        case EPOLLHUP:
+            closeCallback_();
+        break;
+        case EPOLLERR:
+            errorCallback_();
+        break;
+    }
 }
